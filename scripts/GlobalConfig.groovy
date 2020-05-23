db1021b798133a7bacced2f18fc1ae5b5e3b838d
[5by5] - Jean Carlos <jean.carlos@5by5.com.br>
2020-05-23 11:11:00 -0300

    Added jdp ex and refac in script infra as a code

--- scripts/GlobalConfig.groovy
+++ scripts/GlobalConfig.groovy
@@ -6,7 +6,6 @@ import java.net.InetAddress
 def home_dir = System.getenv("JENKINS_HOME")
 GroovyShell shell = new GroovyShell()
 
-def helpers = shell.parse(new File("$home_dir/init.groovy.d/Helpers.groovy"))
 def properties = new ConfigSlurper().parse(new File("$home_dir/config/globals.properties").toURI().toURL())
 
 Jenkins.instance.setNumExecutors(properties.global.numExecutorsOnMaster)
@@ -15,16 +14,8 @@ Jenkins.instance.setScmCheckoutRetryCount(properties.global.scmCheckoutRetryCoun
 
 jlc = JenkinsLocationConfiguration.get()
 
-if (properties.global.jenkinsRootUrl) {
-  jlc.setUrl(properties.global.jenkinsRootUrl)
-} else {
-  def ip = InetAddress.localHost.getHostAddress()
-  jlc.setUrl("http://$ip:8080")
-}
+def ip = InetAddress.localHost.getHostAddress()
+
+jlc.setUrl("http://$ip:8080")
 
 jlc.save()
-
-properties.global.variables.each { key, value ->
-  helpers.addGlobalEnvVariable(Jenkins, key, value)
-}
-
